a231485647e28c0b7c17ece7399a5cd9127221f5
joilsonmslopes <joilsonmirandasl@hotmail.com>
2025-10-19 19:56:37 -0300

    fix: use lombok in CheckingAccount and SavingAccount

--- DigitalBank/src/main/java/com/joilson/DigitalBank/models/SavingAccount.java
+++ DigitalBank/src/main/java/com/joilson/DigitalBank/models/SavingAccount.java
@@ -1,5 +1,10 @@
 package com.joilson.DigitalBank.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SavingAccount extends Account{
     public SavingAccount(Client client) {
         super(client);
